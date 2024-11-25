f1dd5915236d2c915eaf5e1d5c4a658e9909d165
ankitraoak <23078180+ankitraoak@users.noreply.github.com>
2024-11-25 23:49:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7508390450:AAEp39kEUvmtwnmHhgZ2EO6QscIhG9hrRPE")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7416197769:AAGEcM63Uad_IXtRRv-95D4RcTlxc_2U3qw")
     API_ID = int(os.environ.get("API_ID", "20821267"))
     API_HASH = os.environ.get("API_HASH", "8723cdf433be176300044547ee6bab7a")
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://aiitassam:SY06t3delyAShe71@cluster0.ugawa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    AUTH_USERS = [7224758848, 7513565186, 6804641253]
-
-
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Rajahd41:Rajahd41@cluster0.omrv9cu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    AUTH_USERS = [7224758848, 7513565186, 6804641253, 5427627648]
